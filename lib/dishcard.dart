cb4757c2175aeca86360d57303f31511ae5b0b1d
Shubhu69 <shubhankermehta27@gmail.com>
2019-09-20 21:59:27 +0530

    lundkabeej

--- lib/dishcard.dart
+++ lib/dishcard.dart
@@ -14,90 +14,84 @@ class DishCard extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return GestureDetector(
-      onTap: (){
-        print("Tapped");
-      },
+        onTap: () {
+          print("Tapped");
+        },
         child: Card(
-      elevation: 0.0,
-
-      child: Row(
-        children: <Widget>[
-           Container(
-            width: 200,
-            height: 200,
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Image.network(
-                  "$url",
+          elevation: 0.0,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                child: Align(
                   alignment: Alignment.centerLeft,
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Image.network(
+                      "$url",
+                      alignment: Alignment.centerLeft,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Column(children: <Widget>[
+                Text(
+                  "$dish",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Manjari',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 33.0,
+                    color: Colors.black,
+                  ),
+                ),
+                new Divider(),
+                new Divider(),
+                Text("Price : $price",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Manjari',
+                      fontWeight: FontWeight.w100,
+                      fontSize: 30.0,
+                      color: Colors.black,
+                    )),
+                new Divider(),
+                new Divider(),
+                Text(
+                  "Rating : $rating",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Manjari',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30.0,
+                    color: Colors.black,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Star(),
+                    Star(),
+                    Star(),
+                    Star(),
+                    Star(),
+                  ],
+                )
+              ]),
+            ],
           ),
-          Column(children: <Widget>[
-            Text(
-              "$dish",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Manjari',
-                fontWeight: FontWeight.w700,
-                fontSize: 33.0,
-                color: Colors.black,
-              ),
-            ),
-            new Divider(),
-            new Divider(),
-            Text("Price : $price",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Manjari',
-                  fontWeight: FontWeight.w100,
-                  fontSize: 30.0,
-                  color: Colors.black,
-                )),
-            new Divider(),
-            new Divider(),
-            Text(
-              "Rating : $rating",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Manjari',
-                fontWeight: FontWeight.w400,
-                fontSize: 30.0,
-                color: Colors.black,
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Star(),
-                Star(),
-                Star(),
-                Star(),
-                Star(),
-
-                ],
-            )
-          ]),
-        ],
-      ),
-
-    ));
-
+        ));
   }
-
 }
 
-class Star extends StatelessWidget{
+class Star extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Icon(
+    return Icon(
       Icons.star,
       size: 20.0,
       color: Colors.black,
     );
   }
-
 }
