1dccb7b2a8789cc66455cbc151f6ccffee957b37
Shubhu69 <shubhankermehta27@gmail.com>
2019-09-20 16:35:42 +0530

    newtheme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,84 +1,105 @@
 import "package:flutter/material.dart";
 
-var username = "Username";
+var username = 'Username' , mail = 'username@gmail.com';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+      theme: new ThemeData(primaryColor: Colors.white),
       debugShowCheckedModeBanner: false,
+
       title: "Rasoi",
       home: Scaffold(
         drawer: Drawer(
-          child: Container(
-            color: Colors.blueGrey.shade800,
-            margin: EdgeInsets.only(bottom: 650.0),
-            child: ListView(
-              children: <Widget>[
-                Container(margin: const EdgeInsets.all(18.0)),
-                ListTile(
-                  leading: Icon(
-                    Icons.account_circle,
-                    size: 60.0,
-                  ),
-                  title: Text("$username",
-                      style: TextStyle(
-                        fontFamily: 'Manjari',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 33.0,
-                        color: Colors.white,
-                      )),
-                ),
-                Container(margin: const EdgeInsets.all(20.0)),
-                ListTile(
-                  leading: Icon(
-                    Icons.history,
-                    size: 45.0,
-                  ),
-                  title: Text(
-                    "Order History",
+          child: ListView(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+
+                  accountName: Text(
+                    "$username",
                     style: TextStyle(
                       fontFamily: 'Manjari',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 30.0,
-                      color: Colors.blueGrey.shade800,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25.0,
+                      color: Colors.black,
                     ),
                   ),
-                )
-              ],
-            ),
+                accountEmail: Text(
+                "$mail",
+                  style: TextStyle(
+                  fontFamily: 'Manjari',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20.0,
+                  color: Colors.black,
+                  ),
+              ),
+
+                currentAccountPicture: Icon(
+                  Icons.account_circle,
+                  size: 60.0,
+                  color: Colors.black,
+                ),
+
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.history,
+                  size: 45.0,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  "Order History",
+                  style: TextStyle(
+                    fontFamily: 'Manjari',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              new Divider(),
+              ListTile(
+                leading: Icon(
+                  Icons.info_outline,
+                  size: 45.0,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  "About Us",
+                  style: TextStyle(
+                    fontFamily: 'Manjari',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+
+              new Divider(),
+
+              ListTile(
+
+              )
+
+            ],
           ),
-          /*  child: ListView(
-             children: <Widget>[
-               ListTile(
-                 leading: Icon(
-                   Icons.history,
-                   size: 45.0,
-                 ),
-                 title: Text(
-                   "Order History",
-                   style: TextStyle(
-                     fontFamily: 'Manjari',
-                     fontWeight: FontWeight.w400,
-                     fontSize: 30.0,
-                     color: Colors.blueGrey.shade800,
-                   ),
-                 ),
-               ),
-             ],
-           )*/
         ),
         appBar: AppBar(
+          elevation: 0.0,
           title: Text(
             "Rasoi",
+
             style: TextStyle(
               fontFamily: 'Manjari',
               fontWeight: FontWeight.w400,
-              fontSize: 28.0,
-              color: Colors.white,
+              fontSize: 30.0,
+              color: Colors.black,
             ),
           ),
-          backgroundColor: Colors.blueGrey.shade800,
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
         ),
         body: Material(
           color: Colors.white,
